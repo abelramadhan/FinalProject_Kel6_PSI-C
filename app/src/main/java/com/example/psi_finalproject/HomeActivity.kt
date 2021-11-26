6396f21fee2224674abc425338a8e0ce9a786d05
abelramadhan <64635958+abelramadhan@users.noreply.github.com>
2021-11-27 01:22:48 +0700

    Home (belom jadi)

--- app/src/main/java/com/example/psi_finalproject/HomeActivity.kt
+++ app/src/main/java/com/example/psi_finalproject/HomeActivity.kt
@@ -3,7 +3,13 @@ package com.example.psi_finalproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.ktx.database
@@ -12,15 +18,21 @@ import com.google.firebase.ktx.Firebase
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var username_text: TextView
+    private lateinit var layout: LinearLayout
+    private lateinit var hello_text: TextView
+    private lateinit var avatar: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        layout = findViewById(R.id.layout)
+        layout.isVisible = false
 
         auth = Firebase.auth
         var database = Firebase.database
         var user = auth.currentUser
+        hello_text = findViewById(R.id.hello)
+        avatar = findViewById(R.id.avatar_image)
 
         if(user == null) {
             intent = Intent(this, LoginActivity::class.java)
@@ -28,12 +40,18 @@ class HomeActivity : AppCompatActivity() {
             finish()
         }
 
-        username_text = findViewById(R.id.username)
-
         database.getReference("users").child(user!!.uid).child("username").get()
             .addOnSuccessListener {
-                username_text.text = it.value.toString()
+            hello_text.text = "Hello, ${it.value}!"
+                layout.isVisible = true
         }
+
+        database.getReference("users").child(user!!.uid).child("ppicture_url").get()
+            .addOnSuccessListener {
+                Glide.with(this).load(it.value).into(avatar)
+            }
+
+
 
 
 
